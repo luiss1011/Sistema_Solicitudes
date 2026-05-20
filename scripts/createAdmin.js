00a37eea5da8ea726d52ccc03afbd9afeffe847b
luiss1011 <2236000756@utna.edu.mx>
2026-05-20 16:23:39 -0600

    Creación de script para crear usuario admin inicial

---
+++ scripts/createAdmin.js
@@ -0,0 +1,64 @@
+require("dotenv");
+
+const bcrypt = require("bcrypt");
+
+const sequelize = require("../src/config/database");
+
+const Role = require("../src/models/Role");
+const User = require("../src/models/User");
+
+require("../src/models");
+
+async function createAdmin() {
+  try {
+    await sequelize.sync();
+
+    // BUSCAR ROL ADMIN
+    let adminRole = await Role.findOne({
+      where: {
+        nombre: "admin",
+      },
+    });
+
+    // CREAR ROL SI NO EXISTE
+    if (!adminRole) {
+      adminRole = await Role.create({
+        nombre: "admin",
+      });
+    }
+
+    // VERFICAR SI ADMIN YA EXISTE
+    const existingAdmin = await User.findOne({
+      where: {
+        correo: "admin@admin.com",
+      },
+    });
+
+    if (existingAdmin) {
+      console.log("El admin ya existe");
+      process.exit();
+    }
+
+    // ENCRIPTAR PASSWORD
+    const hashedPassword = await bcrypt.hash("123456", 10);
+
+    // CREAR ADMIN
+    await User.create({
+      numeroEmpleado: "0001",
+      nombre: "Administrador",
+      correo: "admin@admin.com",
+      password: hashedPassword,
+      departamento: "Administración",
+      puesto: "Administrador General",
+      releId: adminRole.id,
+    });
+
+    console.log("Admin creado correctamente");
+
+    process.exit();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+createAdmin();
