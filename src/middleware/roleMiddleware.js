c6f199fbec7dcb9b10bc936c0fff5e2a166679d5
luiss1011 <2236000756@utna.edu.mx>
2026-05-25 18:39:17 -0600

    Nueva condición para validar inicio de sesión en roleMiddleware

--- src/middleware/roleMiddleware.js
+++ src/middleware/roleMiddleware.js
@@ -1,12 +1,15 @@
 exports.authorizeRoles = (...roles) => {
-    return (req, res, next) => {
+  return (req, res, next) => {
+    if (!req.session.user) {
+      return res.redirect("/login");
+    }
 
-        const userRole = req.session.user.role;
+    const userRole = req.session.user.role;
 
-        if (!roles.includes(userRole)) {
-            return res.send("Acceso denegado");
-        }
+    if (!roles.includes(userRole)) {
+      return res.status(403).render("errors/403");
+    }
 
-        next();
-    };
+    next();
+  };
 };
